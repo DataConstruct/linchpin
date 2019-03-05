20611a6befc161904864d0bc0490dd121a9b905e
Paul Everton <pattern.paul@gmail.com>
2019-03-05 10:29:05 -0600

    add k8s common deploy templates

--- lib/linchpin/cli.rb
+++ lib/linchpin/cli.rb
@@ -3,6 +3,8 @@ require 'linchpin/templater'
 require 'shellwords'
 require 'find'
 require 'tempfile'
+require 'tmpdir'
+require 'fileutils'
 
 class LinchpinCLI < Thor
   desc "build", "attempt to auto-discover application type and build it."
@@ -26,10 +28,43 @@ class LinchpinCLI < Thor
     command_output = system({}, "bash -c #{escaped_command}")
     exit(1) unless command_output
   end
+
+
+  desc "deploy", "deploy the application"
+  def deploy
+    app_name = discover_appname
+    short_app_name = discover_short_appname
+    app_type = discover_type(Dir.pwd)
+    version = get_version_hash
+
+    temp_dir = Dir.mktmpdir
+    command_output = false
+    begin
+      deploy_templates = Dir[File.join(File.join(File.join(File.dirname(File.expand_path(__FILE__)), 'k8s'), app_type),"*.erb")]
+      deploy_templates.each do |filename|
+        "LOL #{filename}"
+        FileUtils.cp(filename, "#{temp_dir}")
+      end
+      FileUtils.cp(File.join(File.join(File.join(Dir.pwd, 'buildscripts'), "deploy"), "secrets.ejson"), "#{temp_dir}")
+
+      escaped_command = Shellwords.escape("KUBECONFIG=~/.kube/config REVISION=#{version} kubernetes-deploy #{short_app_name} cicd-example --template-dir=#{temp_dir} --bindings=full_name=#{app_name},app_name=#{short_app_name}")
+      command_output = system({}, "bash -c #{escaped_command}")
+
+    ensure
+      # remove the directory.
+      FileUtils.remove_entry temp_dir
+    end
+
+    exit(1) unless command_output
+  end
   private
 
   def discover_type(root_dir)
     'dotnet' if Dir["#{root_dir}/*.sln"].any?
+  end
+
+  def discover_short_appname
+    `git config --get remote.origin.url`.split('/').last.gsub!('.git', '').gsub!('sdlc-example-app-', '').strip
   end
 
   def discover_appname
